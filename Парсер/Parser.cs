79e828c036588b86a9bdb01bd7fab41db10a5f11
Костя <kostapasenko76@gmail.com>
2022-12-06 12:51:44 +0200

    Добавьте файлы проекта.

---
+++ Парсер/Parser.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Парсер
+{
+    internal class Parser
+    {
+        static public void LetsParse(string String, ref int? X, ref int? Y)
+        {
+            string[] words = String.Split(' ');
+            int? x = null;
+            int? y = null;
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (words[i] != " ")
+                {
+                    try
+                    {
+                        if (x == null)
+                        {
+                            x = int.Parse(words[i]);
+                        }
+                        else if (y == null)
+                        {
+                            y = int.Parse(words[i]);
+                        }
+                        else if (x != null && y != null)
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            continue;
+                        }
+                    }
+                    catch
+                    {
+
+                    }
+                }
+            }
+            X = x;
+            Y = y;
+        }
+    }
+}
---
+++ Парсер/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Парсер
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите кординаты: ");
+            string? str = Console.ReadLine();
+            int? X = null;
+            int? Y = null;
+            Parser.LetsParse(str, ref X, ref Y);
+            if (X == null || Y == null)
+            {
+                Console.WriteLine($"ПЕРЕПИСЫВАЙ");
+            }
+            else
+            {
+                Console.WriteLine($"Финальный выход {X} {Y}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
